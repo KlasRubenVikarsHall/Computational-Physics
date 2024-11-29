e89d6741b04eef2f1ba72f94b639bcf86321803b
KlasRubenVikarsHall <rubenh@kth.se>
2024-11-29 16:35:04 +0100

    work:)

--- traveling_salesman.py
+++ traveling_salesman.py
@@ -9,36 +9,57 @@ def initialize_system(M=100): #Creates a town of M houses on an identity square.
     city = [None for j in range(M)]
     for i in range(M):
         city[i] = [rand(), rand()]
+    # for i in range(M):
+    #     plt.scatter(city[i][0],city[i][1])
+    # plt.show()
+    distances = np.zeros((M,M))
     for i in range(M):
-        plt.scatter(city[i][0],city[i][1])
-    plt.show()
-    return city
+        for j in range(M):
+            if i == j:
+                distances[i][j] = 10
+            else:
+                distances[i][j] = metric(city[i], city[j])
+    return city, distances
 
 
-def theoretical_solution(city): #Solve by combinatoric
+def optimal_solution(city): #Solve by combinatoric
     pass
 
 
-def total_length(route):
-    length = 0
-    for i in range(len(route) - 1):
-        length += metric(route[i], route[i + 1])
-    length += metric(route[-1], route[0])
-    return length
-
-
 def metric(a, b):
-    distance = np.sqrt((a[0]-b[0]) ** 2 + (a[1]-b[1]) ** 2)
+    distance = np.sqrt(np.min((np.abs(a[0]-b[0]), 1 - np.abs(a[0]-b[0]))) ** 2 + np.min((np.abs(a[1]-b[1]), 1 - np.abs(a[1]-b[1]))) ** 2) 
     return distance
 
 
-def exchange(route, i, j):
-    a = copy.deepcopy(route[i])
-    b = copy.deepcopy(route[j])
-    route[i] = copy.deepcopy(b)
-    route[j] = copy.deepcopy(a)
+def initialize_route(M, distances):
+    route = [i for i in range(M)]
     return route
 
 
+def nnsolution(M, distances):
+    distances = np.array(distances)
+    route = np.zeros(M,dtype=np.int8)
+    counter = 0
+    current_index = 0
+    while counter < M - 1:
+        counter += 1
+        route[counter] = int(np.argmin(distances[current_index]))
+        distances[:, current_index] = 10
+        current_index = route[counter]
+    return route
+
+
+def exchange(M, prev_route, a, b): # a: cut between a and a+1. b cut between b and b+1
+    new_route = np.zeros(M,dtype=np.int8)
+    new_route[0:a+1] = prev_route[0:a+1]
+    new_route[a+1:b+1] = np.flip(prev_route[a+1:b+1])
+    new_route[b+1:] = prev_route[b+1:]
+    return new_route
+
+
+def delta_E():
 if __name__ == "__main__":
-    initialize_system(5)
+    M = 8
+    city, dist = initialize_system(M)
+    #print(nnsolution(M, dist))
+    print(exchange(M, initialize_route(M, dist), 0, 5))
