3670ea04681e6e4d7b0af66bac613b3fab05515d
KlasRubenVikarsHall <rubenh@kth.se>
2024-12-01 11:59:16 +0100

    annealing & tempering done:)

--- traveling_salesman.py
+++ traveling_salesman.py
@@ -1,10 +1,9 @@
-import copy
-
 import numpy as np
 from numpy.random import rand
 from random import randrange
 from matplotlib import pyplot as plt
 import pickle
+from copy import deepcopy
 
 
 def initialize_system(M=100): #Creates a town of M houses on an identity square.
@@ -98,8 +97,50 @@ def delta_E(M, D, route, a, b):
     return dE
 
 
-def annealing():
-    pass
+def annealing(M, D, route, T_tot, T_step, T_0, r):
+    # M: #of cities
+    # D: Matrix of distances
+    # route
+    # T_tot: Total amount of MCMC steps
+    # T_step: Amount of steps of MC before lowering T
+    # T_0: Initial T
+    # r: Cooling rate, r < 1
+    lengths = []
+    T_protocol = [T_0 * r ** i for i in range(int(T_tot / T_step))]
+    for T in T_protocol:
+        route = MC(T_step, M, T, D, route)
+        lengths.append(length(M, dist, route))
+    plotter = [T_step * (i + 1) for i in range(len(T_protocol))]
+    plt.plot(plotter, lengths)
+    plt.show()
+
+
+def tempering(M, D, route, N_tot, N_step, T_max, T_min, T_n):
+    # N_tot: Total number of MC steps
+    # N_step: number of steps before exchanging temps
+    # T_max: highest T
+    # T_min: lowest temp
+    # T_n: number of simulations at different T
+    T_step = T_max / T_n
+    T_list = [T_min + T_step * i for i in range(T_n)]
+    route_list = [deepcopy(route) for i in range(T_n)]
+    for j in range(int(N_tot / N_step)):
+        for i in range(len(route_list)):
+            route_list[i] = MC(N_step, M, T_list[i], D, route_list[i])
+        # Change places 
+        T_rand = randrange(T_n - 2) # Randomly pick a Temperature between T_0 to T_max - 1
+        ksi = rand()
+        beta = 1 / T_list[T_rand] - 1 / T_list[T_rand + 1]
+        dE = length(M, D, route_list[T_rand]) - length(M, D, route_list[T_rand + 1])
+        if ksi < np.min((1, np.exp(beta * dE))):
+            new_route1 = deepcopy(route_list[T_rand])
+            new_route2 = deepcopy(route_list[T_rand + 1])
+            route_list[T_rand] = new_route2
+            route_list[T_rand + 1] = new_route1
+    len_list = [length(M, D, route_list[i]) for i in range(T_n)]
+    print(len_list)
+    return np.min(len_list)
+    
 
 
 if __name__ == "__main__":
@@ -113,10 +154,13 @@ if __name__ == "__main__":
     M = len(city)
     route_1 = initialize_route(M)
     route_2 = nnsolution(M, dist)
-    route_3 = MC(10000, M, 0.01, dist, route_2)
+    route_3 = MC(10000, M, 0.01, dist, route_1)
+    temp_route = tempering(M, dist, route_1, 1000000, 20000, 0.1, 0.005, 8)
+    #route_annealing = annealing(M, dist, route_1, 1000000, 16000, 0.1, 0.95)
     print(length(M, dist, route_1))
     print(length(M, dist, route_2))
     print(length(M, dist, route_3))
+    print(temp_route)
     # route_nn = nnsolution(M, dist)
     # print(length(M, dist, route_nn))
     # print(exchange(M, initialize_route(M), 0, 5))
